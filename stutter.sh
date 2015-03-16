b842f4ead9c96aa940829e708bf29b752b073a7a
snollygolly <matt.horning@gmail.com>
2015-03-16 13:08:28 -0500

    updated stutter script to use parameters

--- stutter.sh
+++ stutter.sh
@@ -1,17 +1,9 @@
 START_LEVEL=`osascript -e 'get volume settings'| grep -o -E '[0-9]+' | head -1 | sed -e 's/^0\+//'`
 ADJUSTED_LEVEL=`echo "scale=1; $START_LEVEL/100*7" | bc`
-osascript -e 'set volume 0'
-sleep 0.1
-osascript -e "set volume $ADJUSTED_LEVEL"
-sleep 0.1
-osascript -e 'set volume 0'
-sleep 0.1
-osascript -e "set volume $ADJUSTED_LEVEL"
-sleep 0.1
-osascript -e 'set volume 0'
-sleep 0.1
-osascript -e "set volume $ADJUSTED_LEVEL"
-sleep 0.1
-osascript -e 'set volume 0'
-sleep 0.1
-osascript -e "set volume $ADJUSTED_LEVEL"
+for i in `seq 1 $1`;
+do
+  osascript -e 'set volume 0'
+  sleep ${2-0.1}
+  osascript -e "set volume $ADJUSTED_LEVEL"
+  sleep ${2-0.1}
+done
